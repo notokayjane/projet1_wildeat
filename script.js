06093ad5bc0322c276966f78f0f533e16197ece3
Christian Colesnicov <chris.colesnicov@gmail.com>
2024-05-31 09:51:34 +0200

    filter in place for checkboxes, added comments and demo content

--- script.js
+++ script.js
@@ -19,9 +19,9 @@ const restaurants = [
     restaurantSite: "https://www.mima-lyon.fr/",
     restaurantDesc: "MI.MA, à l'origine [MI]lano [MA]rittima, est une ville italienne en Emilia Romagna, où la famille se retrouve. Cuisine italienne, ambiance festive, et sel de Cervia. Une histoire de famille et d'amour pour une cuisine généreuse, conviviale et authentique.",
     restaurantPic: "",
-    dishPic1: "",
-    dishPic2: "",
-    dishPic3: "",
+    dishPic1: "images/epicuriens/Frame 18_20230912170422.png.webp",
+    dishPic2: "images/epicuriens/Frame 19_20230912170424.png.webp",
+    dishPic3: "images/epicuriens/Frame 23_20230912170427.png.webp",
   },
   {
     restaurantName: "Le McQueen",
@@ -31,9 +31,9 @@ const restaurants = [
     restaurantSite: "https://lemcqueen.fr/",
     restaurantDesc: "Au McQueen, café & bar à cocktails japonais à l’Opéra, dégustez ramen, donburi, gyozas, desserts, thés, vins naturels italiens, sakés, et cocktails maison. Ambiance musicale rock par Kaoru, qui se trouve être le barman !",
     restaurantPic: "",
-    dishPic1: "",
-    dishPic2: "",
-    dishPic3: "",
+    dishPic1: "images/epicuriens/Frame 18_20230912170422.png.webp",
+    dishPic2: "images/epicuriens/Frame 19_20230912170424.png.webp",
+    dishPic3: "images/epicuriens/Frame 23_20230912170427.png.webp",
   },
   {
     restaurantName: "Le chalet Savoyard",
@@ -43,9 +43,9 @@ const restaurants = [
     restaurantSite: "https://www.chalet-savoyard.fr/",
     restaurantDesc: "En plein cœur de Paris, dans le 11ème, ce restaurant chaleureux attire les amoureux de gastronomie savoyarde authentique avec des produits du terroir. Spécialités copieuses, créations maison, accueil convivial.",
     restaurantPic: "",
-    dishPic1: "",
-    dishPic2: "",
-    dishPic3: "",
+    dishPic1: "images/epicuriens/Frame 18_20230912170422.png.webp",
+    dishPic2: "images/epicuriens/Frame 19_20230912170424.png.webp",
+    dishPic3: "images/epicuriens/Frame 23_20230912170427.png.webp",
   },
   {
     restaurantName: "Pidè Paris",
@@ -55,9 +55,9 @@ const restaurants = [
     restaurantSite: "https://pide.paris/",
     restaurantDesc: "Pidè Paris propose une version turque de la pizza, le pide, avec des ingrédients frais et authentiques. Le menu inclut pides, mezzés et desserts traditionnels, le tout dans une ambiance conviviale et chaleureuse.",
     restaurantPic: "",
-    dishPic1: "",
-    dishPic2: "",
-    dishPic3: "",
+    dishPic1: "images/epicuriens/Frame 18_20230912170422.png.webp",
+    dishPic2: "images/epicuriens/Frame 19_20230912170424.png.webp",
+    dishPic3: "images/epicuriens/Frame 23_20230912170427.png.webp",
   },
   {
     restaurantName: "BIBIBAP",
@@ -67,9 +67,9 @@ const restaurants = [
     restaurantSite: "https://www.bibibap.fr/",
     restaurantDesc: "Découvrez le meilleur de la streetfood coréenne autour de plats traditionnels comme le bibimbap et de tapas originales à partager sans modération !",
     restaurantPic: "",
-    dishPic1: "",
-    dishPic2: "",
-    dishPic3: "",
+    dishPic1: "images/epicuriens/Frame 18_20230912170422.png.webp",
+    dishPic2: "images/epicuriens/Frame 19_20230912170424.png.webp",
+    dishPic3: "images/epicuriens/Frame 23_20230912170427.png.webp",
   },
   {
     restaurantName: "Mochicas Café",
@@ -79,9 +79,9 @@ const restaurants = [
     restaurantSite: "https://mochicascafe.com/",
     restaurantDesc: "Le restaurant Mochicas propose une cuisine péruvienne traditionnelle et fusionnée. Le menu comprend le ceviche classique, le lomo saltado, les choritos a la chalaca, les empanadas et la causa limeña.",
     restaurantPic: "",
-    dishPic1: "",
-    dishPic2: "",
-    dishPic3: "",
+    dishPic1: "images/epicuriens/Frame 18_20230912170422.png.webp",
+    dishPic2: "images/epicuriens/Frame 19_20230912170424.png.webp",
+    dishPic3: "images/epicuriens/Frame 23_20230912170427.png.webp",
   },
   {
     restaurantName: "Royal Orchid",
@@ -91,9 +91,9 @@ const restaurants = [
     restaurantSite: "https://www.facebook.com/p/Royal-Orchid-100027943136673/",
     restaurantDesc: "Le Royal Orchid à Montpellier propose une cuisine thaïlandaise authentique et raffinée. La carte variée comprend des plats populaires tels que le pad thaï, les currys thaï, les brochettes satay, ainsi que des spécialités moins connues comme le poisson à la sauce aigre-douce ou le bœuf loc lac.",
     restaurantPic: "",
-    dishPic1: "",
-    dishPic2: "",
-    dishPic3: "",
+    dishPic1: "images/epicuriens/Frame 18_20230912170422.png.webp",
+    dishPic2: "images/epicuriens/Frame 19_20230912170424.png.webp",
+    dishPic3: "images/epicuriens/Frame 23_20230912170427.png.webp",
   },
   {
     restaurantName: "Le Quatrième Mur",
@@ -103,9 +103,9 @@ const restaurants = [
     restaurantSite: "https://quatrieme-mur.com/",
     restaurantDesc: "Au théâtre, le quatrième mur est celui, invisible, qui sépare le public de la scène. Un nom tout choisi pour cette table installée dans les ors du Grand théâtre ! Un produit de qualité, une cuisson précise, une garniture et un jus : Philippe Etchebest va à l'essentiel et nous régale en toute simplicité. Installée sur une galerie latérale du grand théâtre, la terrasse est très agréable",
     restaurantPic: "",
-    dishPic1: "",
-    dishPic2: "",
-    dishPic3: "",
+    dishPic1: "images/epicuriens/Frame 18_20230912170422.png.webp",
+    dishPic2: "images/epicuriens/Frame 19_20230912170424.png.webp",
+    dishPic3: "images/epicuriens/Frame 23_20230912170427.png.webp",
   },
 ]
 
@@ -126,17 +126,15 @@ function closeNav() {
   sidenav.classList.remove("active");
 }
 
+// Slideshow et boutons associés
 var slideIndex = 1;
 showDivs(slideIndex);
-
 function plusDivs(n){
   showDivs(slideIndex += n);
 }
-
 function currentDiv(n){
   showDivs(slideIndex = n);
 }
-
 function showDivs(n){
   var i;
   var x = document.getElementsByClassName("mySlides");
@@ -152,3 +150,81 @@ function showDivs(n){
   x[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " w3-white";
 }
+// Fin du slideshow
+
+// Fonction pour filtrer les restaurants
+function filterRestaurants(restaurants, filtres) {
+  return restaurants.filter(restaurant => {
+    // On fait une const pour chaque type de filtre
+    // On regarde si un filtre est coché ou pas (!filtre.XXX permet de vérifier si c'est vide/faux ou pas)
+    // et si c'est bien vide alors on regarde ce qui est dans le restaurant.xxx après
+    const matchVille = !filtres.ville || filtres.ville.includes(restaurant.city);
+    const matchTypeCuisine = !filtres.typeCuisine || filtres.typeCuisine.includes(restaurant.restaurantType);
+    const matchNote = !filtres.note || (filtres.note.includes("x étoiles")) || filtres.note.includes(restaurant.restaurantRating);
+    return matchVille && matchTypeCuisine && matchNote;
+  });
+}
+
+
+// On sélectionne toutes les checkbox
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+// On affiche les restaurants dans le container fait pour
+function afficherRestaurants(restaurants) {
+  const container = document.getElementById('restaurant-container');
+  // reset du container, bug si non présent
+  container.innerHTML = '';
+  // boucle for qui :
+  // créé une div nommé card en html, y ajoute la class vignette-restaurant, et écrit le code avec innerHTML dans le HTML.
+  // le code HTML est ici copié du HTML d'origine, en remplaçant les éléments exemples par les variables associées et issues
+  // de restaurant : restaurant.xxx
+  for (const restaurant of restaurants) {
+    const card = document.createElement('div');
+    card.classList.add('vignette-restaurant');
+    card.innerHTML = `<div class="information-restaurant">
+                              <h2 class="restaurant-name">${restaurant.restaurantName}</h2>
+                              <p class="restaurant-description">${restaurant.restaurantDesc}</p>
+                              <input type="checkbox" class="expand-button">
+                              <div class="restaurant-note">Note: ${restaurant.restaurantRating}</div>
+                              <div class="restaurant-link"><a href="${restaurant.restaurantSite}" target="_blank">Site web</a></div>
+                          </div>
+                          <div class="image-restaurant">
+                              <img src="${restaurant.restaurantPic}" class="mySlides" alt="photo restaurant">
+                              <img src="${restaurant.dishPic1}" class="mySlides" alt="photo plat 1">
+                              <img src="${restaurant.dishPic2}" class="mySlides" alt="photo plat 2">
+                              <img src="${restaurant.dishPic3}" class="mySlides" alt="photo plat 3">
+                              <div class="slide-buttons">
+                                <div class="left-arrow"></div>
+                                <span class="select-button" onclick="currentDiv(1)"></span>
+                                <span class="select-button" onclick="currentDiv(2)"></span>
+                                <span class="select-button" onclick="currentDiv(3)"></span>
+                                <div class="right-arrow"></div>
+                              </div>
+                          </div>`;
+    container.appendChild(card);
+  }
+}
+
+// Mise à jour des résultats
+function updateFilteredRestaurants() {
+  const filtre = {};
+    // On cherche les valeurs des cases cochées, nom et état coché/décoché
+  checkboxes.forEach(checkbox => {
+      if (checkbox.checked) {
+        const name = checkbox.name;
+        if (filtre[name]) {filtre[name].push(checkbox.value);
+        } else 
+        {filtre[name] = [checkbox.value];
+        }}});
+    // Filtre et maj de la liste
+    const restaurantsFiltres = filterRestaurants(restaurants, filtre);
+    afficherRestaurants(restaurantsFiltres);
+  }
+
+  // Update sur case cochée
+checkboxes.forEach(checkbox => {
+  checkbox.addEventListener('change', updateFilteredRestaurants);
+});
+  
+afficherRestaurants(restaurants);
+  
